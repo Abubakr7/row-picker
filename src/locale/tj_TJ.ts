2811b280c852db750a062359323bacc1f4d227db
Abubakr7 <abubakr.juraev98@gmail.com>
2024-07-08 16:25:05 +0500

    add tajik language

---
+++ src/locale/tj_TJ.ts
@@ -0,0 +1,32 @@
+import type { Locale } from '../interface';
+
+const locale: Locale = {
+    locale: 'tj_TJ',
+    today: 'Имруз',
+    now: 'Ҳозир',
+    backToToday: 'Санаи ҷорӣ',
+    ok: 'ОК',
+    clear: 'Тоза кардан',
+    month: 'Моҳ',
+    year: 'Сол',
+    timeSelect: 'Интихоби вакт',
+    dateSelect: 'Интихоби сана',
+    monthSelect: 'Интихоби моҳ',
+    yearSelect: 'Интихоби сол',
+    decadeSelect: 'Интихоби даҳсола',
+    yearFormat: 'YYYY',
+    dateFormat: 'DD-MM-YYYY',
+    dayFormat: 'D',
+    dateTimeFormat: 'DD-MM-YYYY HH:mm:ss',
+    monthBeforeYear: true,
+    previousMonth: 'Моҳи гузашта (PageUp)',
+    nextMonth: 'Моҳи оянда (PageDown)',
+    previousYear: 'Соли гузашта (Control + left)',
+    nextYear: 'Соли оянда (Control + right)',
+    previousDecade: 'Даҳсолаи гузашта',
+    nextDecade: 'Даҳсолаи оянда',
+    previousCentury: 'Асри гузашта',
+    nextCentury: 'Асри оянда',
+};
+
+export default locale;
